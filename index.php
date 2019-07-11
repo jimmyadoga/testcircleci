f724cb918db81c9afa45851320b83f27a4bb6bf9
james Adoga <jjjadoga@yahoo.com>
2019-07-11 17:09:32 +0100

    variable added

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
+<?php
+
+$name = "jimmy";
+
+?>
+
 <html>
     <head>
         <title>Test circle ci</title>
     </head>
     <body>
-        <h1>hello</h1>
+        <h1><?php echo $name ; ?></h1>
 		<h2>hello</h2>
     </body>
 </html>
